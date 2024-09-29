dc56d8a979fab516afe1c65898ac00ba85b532bc
Sangam Arora <arorasam2109@gmail.com>
2024-09-29 16:46:16 +0530

    superuse page

--- frontend/src/components/superuser.js
+++ frontend/src/components/superuser.js
@@ -12,7 +12,7 @@ import { REACT_APP_BACKEND_URL } from '../utils/constants';
 const SuperUser = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
-  const [verification,setVerification]=useState(false)
+  const [verification, setVerification] = useState(false)
   const navigate = useNavigate()
   const [drivers, setDrivers] = useState([
   ]);
@@ -75,7 +75,7 @@ const SuperUser = () => {
         setVerification(status)
         if (status) {
           toast.success("Driver Approved")
-          
+
         } else {
           toast.success("Driver Rejected")
         }
@@ -87,91 +87,87 @@ const SuperUser = () => {
   };
 
   return (
-    <div className= "bg-yellow-100 min-h-screen">
+    <div className="bg-yellow-100 min-h-screen">
       {/* Header */}
-      <Navbar/>
+      <Navbar />
 
       {/* Verification Section */}
       <div className=" mx-4">
-      <section className="mt-8">
-        <h2 className="text-xl font-semibold text-black-600 mb-4">Verify Drivers and Documents</h2>
-        <div className="flex space-x-4 mb-6">
-          <input
-            type="text"
-            placeholder="Search Driver by Name or ID"
-            className="p-4 w-80 bg-gray-100 border-2 border-gray-300 rounded"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button
-            onClick={handleSearch}
-            className="bg-primaryOrange-light hover:bg-primaryOrange text-white font-semibold py-2 px-4 rounded"
-          >
-            Search
-          </button>
-        </div>
+        <section className="mt-8">
+          <h2 className="text-xl font-semibold text-black-600 mb-4">Verify Drivers and Documents</h2>
+          <div className="flex space-x-4 mb-6">
+            <input
+              type="text"
+              placeholder="Search Driver by Name or ID"
+              className="p-4 w-80 bg-gray-100 border-2 border-gray-300 rounded"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button
+              onClick={handleSearch}
+              className="bg-primaryOrange-light hover:bg-primaryOrange text-white font-semibold py-2 px-4 rounded"
+            >
+              Search
+            </button>
+          </div>
 
-        {/* Drivers Table */}
-        <div className='overflow-x-auto w-full'>
-        <table className="w-full bg-white rounded-lg shadow-md">
-          <thead className="bg-gray-100">
-              <tr>
-                <th className="py-3 px-6">FaceID</th>
-                <th className="py-3 px-6">Name</th>
-                <th className="py-3 px-6">Age</th>
-                <th className="py-3 px-6">Gender</th>
-                <th className="py-3 px-6">Phone No.</th>
-                <th className="py-3 px-6">License No.</th>
-                <th className="py-3 px-6">License Document</th>
-                <th className="py-3 px-6">Status</th>
-                <th className="py-3 px-6">Action</th>
-              </tr>
-          </thead>
-          <tbody>
-            {drivers.map((driver) => (
-              <tr key={driver.id} className="border-b text-center">
-                <td className="py-3 px-6">
-                  <button onClick={() => handleViewCertificate(driver.faceIDPhoto)} className="text-blue-600">View</button>
-                </td>
-                <td className="py-3 px-6">{driver.name}</td>
-                <td className="py-3 px-6">{driver.age}</td>
-                <td className="py-3 px-6">{driver.gender}</td>
-                <td className="py-3 px-6">{driver.phone}</td>
-                <td className="py-3 px-6">{driver.age}</td>
-                <td className="py-3 px-6">{driver.gender}</td>
-                <td className="py-3 px-6">{driver.phone}</td>
-                <td className="py-3 px-6">{driver.licenseNumber}</td>
-                <td className="py-3 px-6">
-                  <button onClick={() => handleViewCertificate(driver.licensePhotograph)} className="text-blue-600">View</button>
+          {/* Drivers Table */}
+          <div className='overflow-x-auto w-full'>
+            <table className="w-full bg-white rounded-lg shadow-md">
+              <thead className="bg-gray-100">
+                <tr>
+                  <th className="py-3 px-6">FaceID</th>
+                  <th className="py-3 px-6">Name</th>
+                  <th className="py-3 px-6">Age</th>
+                  <th className="py-3 px-6">Gender</th>
+                  <th className="py-3 px-6">Phone No.</th>
+                  <th className="py-3 px-6">License No.</th>
+                  <th className="py-3 px-6">License Document</th>
+                  <th className="py-3 px-6">Status</th>
+                  <th className="py-3 px-6">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {drivers.map((driver) => (
+                  <tr key={driver.id} className="border-b text-center">
+                    <td className="py-3 px-6">
+                      <button onClick={() => handleViewCertificate(driver.faceIDPhoto)} className="text-blue-600">View</button>
+                    </td>
+                    <td className="py-3 px-6">{driver.name}</td>
+                    <td className="py-3 px-6">{driver.age}</td>
+                    <td className="py-3 px-6">{driver.gender}</td>
+                    <td className="py-3 px-6">{driver.phone}</td>
+                    <td className="py-3 px-6">{driver.licenseNumber}</td>
+                    <td className="py-3 px-6">
+                      <button onClick={() => handleViewCertificate(driver.licensePhotograph)} className="text-blue-600">View</button>
 
-                </td>
-               
-                <td className="py-3 px-6">{driver.isVerified ? "Approved" : "Not Approved"}</td>
-                <td className="py-3 px-6 space-x-2">
-                  {!driver.isVerified ?
-                    <button
-                      className="bg-primaryOrange-light hover:bg-primaryOrange text-white font-semibold py-2 px-4 rounded"
-                      onClick={() => handleApprove(driver._id, true)}
-                    >
-                      Approve
-                    </button> : null}
+                    </td>
 
-                  <button
-                    className="bg-primaryOrange-light hover:bg-primaryOrange text-white font-semibold py-2 px-4 rounded"
-                    onClick={() => handleApprove(driver._id, false)}
-                  >
-                    Reject
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        </div>
-        </div>
-      </section>
+                    <td className="py-3 px-6">{driver.isVerified ? "Approved" : "Not Approved"}</td>
+                    <td className="py-3 px-6 space-x-2">
+                      {!driver.isVerified ?
+                        <button
+                          className="bg-primaryOrange-light hover:bg-primaryOrange text-white font-semibold py-2 px-4 rounded"
+                          onClick={() => handleApprove(driver._id, true)}
+                        >
+                          Approve
+                        </button> : null}
 
-      {/* Search Section 
+                      <button
+                        className="bg-primaryOrange-light hover:bg-primaryOrange text-white font-semibold py-2 px-4 rounded"
+                        onClick={() => handleApprove(driver._id, false)}
+                      >
+                        Reject
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </section>
+
+        {/* Search Section 
       <section className="mt-12">
         <h2 className="text-xl font-semibold text-black-600 mb-4">Search Users, Vehicles, or Rides</h2>
         <div className="flex space-x-4 mb-6">
@@ -204,11 +200,9 @@ const SuperUser = () => {
         </div>
       </section>
       */}
-      <ToastContainer />
+        <ToastContainer />
       </div>
     </div>
-    </>
-    
   );
 };
 
