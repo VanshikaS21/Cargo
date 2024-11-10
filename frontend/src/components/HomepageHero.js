8038edb102954d13ff10c7437b385f83a48b48f0
Rahul Pandey <rahul.Pand3y@gmail.com>
2024-11-10 22:30:16 +0530

    blank screen fix

--- frontend/src/components/HomepageHero.js
+++ frontend/src/components/HomepageHero.js
@@ -426,7 +426,7 @@ function HomepageHero({ rides, setRides }) {
         Distance: {distance} km
       </div>
       {role !== "Driver" && (
-  rides.length === 0 ? (
+  rides?.length === 0 ? (
     <div>No Rides Found</div>
   ) : (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 bg-gray-200 p-10 rounded-lg" style={{ zIndex: 999 }} >
